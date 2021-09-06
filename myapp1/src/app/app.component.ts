9ac6139c4f31f975bc98d128daff153dbab38c9d
ivantica <ivan.robfer@gmail.com>
2021-09-06 15:36:52 -0500

    segundo commit

--- myapp1/src/app/app.component.ts
+++ myapp1/src/app/app.component.ts
@@ -11,6 +11,20 @@ export class AppComponent {
   mensaje="";
   nombre:string="";
   apellido:string="";
+  //titulo:string="";
+  mispasiones: any[];
+  misemprendimientos:any[];
+  misamores:any=[{"nombre":"Luz"},{"nombre":"Hernan"},{"nombre":"silverio"},{"nombre":"Liliana"}];
+  constructor(){
+    this.mispasiones=[
+     {"titulo":"Java SPring Framework"},
+     {"titulo":"ASP.NET Core"},
+     {"titulo":"Angular JS"},
+     {"titulo":"AWS Devops"},
+     {"titulo":"Requerimientos de Software"},   
+    ];
+    this.misemprendimientos=[{titulo:"Empresa Experta en Desarrollo de Software con Java y ASP Net Core"},{titulo:"Empresa inmobiliaria experta en Inversiones en Zonas Rurales"},{titulo:"Plataforma de intermediación entre Productores y Clientes finales"}];
+  }
 
   registrarUsuario(){
     this.registrado =true;
